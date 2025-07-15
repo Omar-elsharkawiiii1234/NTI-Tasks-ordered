ef5b5c55a34e7c9a3b7128bcd279fbe8ac4d96ea
Omar-elsharkawiiii1234 <150117099+Omar-elsharkawiiii1234@users.noreply.github.com>
2025-07-15 14:28:19 +0300

    Add files via upload

---
+++ Day 2/T8/main.js
@@ -0,0 +1,8 @@
+function PrintName(){
+    let username=document.getElementById("username").value;
+    document.getElementById("output").innerHTML="Hello "+username+",We Are Pleasure To See You Today.";
+    document.getElementById("output").style.color="white";
+    document.getElementById("output").style.backgroundColor="red";
+    document.getElementById("output").style.padding="10px 20px";
+    document.getElementById("output").style.borderRadius="10px";
+}
