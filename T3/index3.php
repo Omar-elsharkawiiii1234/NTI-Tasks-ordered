19b2849be4913329f5822c4aad4ca1131c9d917e
Omar-elsharkawiiii1234 <150117099+Omar-elsharkawiiii1234@users.noreply.github.com>
2025-07-16 14:22:42 +0300

    adding tasks of day three

---
+++ T3/index3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task One Day 3 php</title>
+</head>
+<body>
+     <?php
+
+        // task one ==>
+        echo "omar ahmed elsharkawi<br>";
+        print "اول سطر لي باستخدام php<br>";
+        echo "<hr>";
+        // task two==>
+        $n1=12;
+        $n2=10;
+        echo "the summtion is:".($n1 +$n2)."<br>";
+        echo "the change is:".($n1 %$n2)."<br>";
+
+     ?>
+</body>
+</html>
